d54cca01e904b658d431f51baafc648f9665f50a
ismail <ismailbrahimi222000@gmail.com>
2022-01-04 16:21:20 +0100

    push dl ferrari

--- src/main/java/com/example/gestion_image/HelloController.java
+++ src/main/java/com/example/gestion_image/HelloController.java
@@ -7,5 +7,12 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
+    @FXML
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+    fileChooser.getExtensionFilters().add(extFilter);
+    File file = fileChooser.showOpenDialog(primaryStage);
+    System.out.println(file);
+
 //
 }
