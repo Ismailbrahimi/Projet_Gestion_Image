6dbae24d06cab379f02d647822b3620c6e306a3f
ismail <ismailbrahimi222000@gmail.com>
2022-01-04 18:02:39 +0100

    push Filechooser

--- src/main/java/com/example/gestion_image/HelloController.java
+++ src/main/java/com/example/gestion_image/HelloController.java
@@ -1,18 +1,43 @@
 package com.example.gestion_image;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    //creation de l'objet filechooser
+    final FileChooser fc = new FileChooser();
 
     @FXML
-    FileChooser fileChooser = new FileChooser();
-    FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
-    fileChooser.getExtensionFilters().add(extFilter);
-    File file = fileChooser.showOpenDialog(primaryStage);
-    System.out.println(file);
+    private Button btnOpenImgFile;
 
-//
+    @FXML
+    private ImageView ivFiles;
+
+    @FXML
+    public void handleBtnOpenImgFile(ActionEvent event)
+    {
+    fc.setTitle("Title");
+    fc.setInitialDirectory(new File("C:\\Users\\ps42\\IdeaProjects\\Gestion_Image\\Ressources"));
+    fc.getExtensionFilters().clear();
+    fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files","*.png","*.jpg","*.gif"));
+
+    File file = fc.showOpenDialog(null);
+    if(file!=null)
+    {
+        ivFiles.setImage(new Image(file.toURI().toString()));
+    }
+    else
+    {
+        System.out.println("A file is invalid");
+    }
+    }
 }
