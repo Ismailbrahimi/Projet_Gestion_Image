38a4f885e8c81f88ae1ea061c838fc8eb9496d5e
mohssineaitboukdir@gmail.com <MohssineGit2829>
2022-01-05 03:28:14 +0100

    Moooooohssine

--- src/main/java/com/example/gestion_image/HelloController.java
+++ src/main/java/com/example/gestion_image/HelloController.java
@@ -3,8 +3,6 @@ package com.example.gestion_image;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
