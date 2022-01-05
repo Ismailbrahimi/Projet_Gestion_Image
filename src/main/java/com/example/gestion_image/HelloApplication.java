241ad43af8057fb1b21ff3828ce8c204bd1a2017
ismail <ismailbrahimi222000@gmail.com>
2022-01-05 01:56:49 +0100

    push commit avec erreur sepia et erreur boutton filtres

--- src/main/java/com/example/gestion_image/HelloApplication.java
+++ src/main/java/com/example/gestion_image/HelloApplication.java
@@ -3,21 +3,28 @@ package com.example.gestion_image;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Accueil.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 920, 540);
-        stage.setTitle("Hello!");
+        stage.setTitle("Gestion d'image");
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
 }
+
+
