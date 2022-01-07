4ced574fa6e972c5e23f6417fa7b081045eee390
mohssineaitboukdir@gmail.com <MohssineGit2829>
2022-01-07 02:48:10 +0100

    Priwett

--- src/main/java/com/example/gestion_image/HelloController.java
+++ src/main/java/com/example/gestion_image/HelloController.java
@@ -9,12 +9,21 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import java.io.File;
+
+import static java.lang.Math.round;
 
 public class HelloController {
 
     //creation de l'objet filechooser
     public FileChooser fc = new FileChooser();
+    public File file;
     public  Image img;
     int h;
     int w;
@@ -25,7 +34,8 @@ public class HelloController {
     @FXML
     private ImageView ivFiles;
 
-
+    @FXML
+    private ImageView ivFiles2;
 
     @FXML
     private Button myLeft;
@@ -66,7 +76,7 @@ public class HelloController {
     fc.getExtensionFilters().clear();
     fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files","*.png","*.jpg","*.gif"));
 
-    File file = fc.showOpenDialog(null);
+     file = fc.showOpenDialog(null);
     if(file!=null)
     {
         ivFiles.setImage(new Image(file.toURI().toString()));
@@ -80,11 +90,11 @@ public class HelloController {
     }
     }
 
-
     public void handleOriginale() {
 
         ivFiles.setImage(img);
     }
+
     public void handleNoireBlanc() {
 
         WritableImage imgOUT = new WritableImage(w,h);
@@ -106,7 +116,7 @@ public class HelloController {
 
         for(int i = 0; i < h; i++) {
             for(int j = 0; j < w; j++) {
-                Color colorImgIN1 = img.getPixelReader().getColor(j,i) ;
+                Color colorImgIN1 = img.getPixelReader().getColor(j,i);
                 Color colorImgOUT1 =new Color(colorImgIN1.getBlue(),colorImgIN1.getRed(),colorImgIN1.getGreen(),1);
                 imgOUT1.getPixelWriter().setColor(j,i,colorImgOUT1);
             }
@@ -165,5 +175,75 @@ public class HelloController {
         }
         ivFiles.setImage(imgOUT2);
     }
+
+    // -----------------------------
+    // cette methode nous retourne le niveau du Gris
+    public  int  niveauGris(int couleur) {
+        int blue = (couleur) & 0xff;
+        int green = (couleur >> 8) & 0xff;
+        int red = (couleur >> 16) & 0xff;
+        //https://en.wikipedia.org/wiki/Grayscale
+        return ((green + red + blue) / 3);
+    }
+
+    public void handlePrewitt() throws IOException {
+
+        //lire l'image en format Buffered
+        BufferedImage bImg = ImageIO.read(file);
+        int grade = -1;
+        int[][] PriwettColors = new int[w][h];
+        double echelle=0;
+
+        for (int i = 1; i < w - 1; i++) {
+            for (int j = 1; j < h - 1; j++) {
+
+                // on cherche le niveau du Gris
+                int niv []=
+                        {
+                 niveauGris(bImg.getRGB(i - 1, j - 1)), niveauGris(bImg.getRGB(i - 1, j)), niveauGris(bImg.getRGB(i - 1, j + 1)),
+                 niveauGris(bImg.getRGB(i, j - 1)), niveauGris(bImg.getRGB(i, j)), niveauGris(bImg.getRGB(i, j + 1)),
+                 niveauGris(bImg.getRGB(i + 1, j - 1)), niveauGris(bImg.getRGB(i + 1, j)), niveauGris(bImg.getRGB(i + 1, j + 1))
+                        };
+
+                //Les operateurs de Prewitt
+                // Prewitt Horizontal
+                int PrewittX =  ((1 * niv[0]) + (0 * niv[1]) + (-1 * niv[2]))  +  ((1 * niv[3]) + (0 * niv[4]) + (-1 * niv[5]))  +  ((1 * niv[6]) + (0 * niv[7]) + (-1 * niv[8]));
+                // Prewitt Vertical
+                int PrewittY =  ((1 * niv[0]) + (1 * niv[1]) + (1 * niv[2]))  +  ((0 * niv[3]) + (0 * niv[4]) + (0 * niv[5]))  +  ((-1 * niv[6]) + (-1 * niv[7]) + (-1 * niv[8]));
+
+                // On calcule 'Hypotenuse' c=sqrt(a*a + b*b)
+                int Hypotenuse = (int) Math.sqrt((PrewittX * PrewittX) + (PrewittY * PrewittY));
+
+                if(Hypotenuse > grade) {
+                    grade = Hypotenuse;
+                }
+
+                PriwettColors[i][j] = Hypotenuse;
+            }
+        }
+
+        echelle = (double)(255.0/grade );
+
+        for (int i = 1; i < w - 1; i++) {
+            for (int j = 1; j < h - 1; j++) {
+                int Pcolor = (int)(PriwettColors[i][j] * echelle);
+                //On calcule la nouvelle combinaison RGB
+                Pcolor = 0xff000000 | (Pcolor << 16) | (Pcolor << 8) | Pcolor;
+                bImg.setRGB(i, j, Pcolor);
+            }
+        }
+
+
+        // On ne sait pas comment convertir Buffered Image to Image directement, du coup on a l'enregiste comme un fichier(jpg) apres on la lit (Ca peut etre con)
+        File PfileOUT = new File("PriwettIMAGE.jpg");
+
+        ImageIO.write(bImg, "png", PfileOUT);
+
+        File PfileIN = new File("PriwettIMAGE.jpg");
+
+        Image PriwettIMAGE =new Image(PfileIN.toURI().toString());
+        ivFiles.setImage(PriwettIMAGE);
+    }
+
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.gestion_image {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.gestion_image to javafx.fxml;
