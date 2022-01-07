5749eb0df469bf0f6d8a995ac4b4e4032a38a3d3
ismail <ismailbrahimi222000@gmail.com>
2022-01-07 22:14:08 +0100

    push fichier xml

---
+++ src/main/java/com/example/gestion_image/Image.java
@@ -0,0 +1,41 @@
+package com.example.gestion_image;
+
+public class Image
+{
+    private String marque;
+    private String modele ;
+    private String couleur ;
+    private int annee ;
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    public int getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(int annee) {
+        this.annee = annee;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,16 @@ module com.example.gestion_image {
 
     requires jakarta.xml.bind;
     requires com.sun.xml.bind;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.dataformat.xml;
 
     opens com.example.gestion_image to javafx.fxml;
     exports com.example.gestion_image;
 }
+
+
+
+
+
+
