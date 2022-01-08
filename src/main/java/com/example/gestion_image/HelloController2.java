30e362138e71595b5040e533f61175ea44922dcd
mohssineaitboukdir@gmail.com <MohssineGit2829>
2022-01-08 19:48:23 +0100

    interfaaaaaces

---
+++ src/main/java/com/example/gestion_image/HelloController2.java
@@ -0,0 +1,66 @@
+package com.example.gestion_image;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+
+public class HelloController2 {
+
+    //creation de l'objet filechooser
+    public Parent root;
+    public Stage stage;
+    public Scene scene;
+    public String selected;
+
+
+    public ImageView ivFiles;
+
+    private AnchorPane listImg;
+
+       public void redirectToFilters(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+           ImageView iv = (ImageView) mouseEvent.getSource();
+           System.out.println(" IMAGE VIEW : "+iv.getImage());
+        selected = (String) mouseEvent.getSource().toString();
+        System.out.println(selected);
+
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("Accueil.fxml"));
+        root= loader.load();
+        HelloController hw1 = loader.getController();
+        //hw1.setPreloadedImage("Ressources\\NissanGTR.jpg");
+           hw1.setPreloadedImage2(iv.getImage());
+
+
+           stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+           scene = new Scene(root);
+           stage.setScene(scene);
+           stage.show();
+
+
+    }
+}
